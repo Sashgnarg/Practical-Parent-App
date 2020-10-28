4480d329aa71c850747b488c813802a15c6db46a
CooLguNXDD:) <andrewliang0123@gmail.com>
2020-10-28 12:04:52 -0700

    initial the project

---
+++ app/src/main/java/com/cmpt276/iteration1practicalparent/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cmpt276.iteration1practicalparent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
