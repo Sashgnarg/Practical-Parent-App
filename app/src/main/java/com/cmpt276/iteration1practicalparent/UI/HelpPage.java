621baebb4472f8181fc5436edadeb5ceff4bbfd3
hwl10 <oscar_liu@sfu.ca>
2020-11-20 13:11:30 -0800

    basic help page finished

--- app/src/main/java/com/cmpt276/iteration1practicalparent/UI/HelpPage.java
+++ app/src/main/java/com/cmpt276/iteration1practicalparent/UI/HelpPage.java
@@ -3,8 +3,12 @@ package com.cmpt276.iteration1practicalparent.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.cmpt276.iteration1practicalparent.R;
+
+import org.w3c.dom.Text;
 
 import java.util.Objects;
 
@@ -16,6 +20,15 @@ public class HelpPage extends AppCompatActivity {
         setContentView(R.layout.activity_help_page);
         setTitle("Help");
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        // linking homepage background
+        TextView home = (TextView) findViewById(R.id.homePageBackground);
+        home.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // linking timer background
+        TextView time = (TextView) findViewById(R.id.timerBack);
+        home.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 
 }
