9b8df025f1ccef3d375414deb78daf18781e4e21
Mariz Fahmy <marizf@sfu.ca>
2020-11-19 16:49:29 -0800

    help button + tasks button

---
+++ app/src/main/java/com/cmpt276/iteration1practicalparent/UI/HelpPage.java
@@ -0,0 +1,17 @@
+package com.cmpt276.iteration1practicalparent.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cmpt276.iteration1practicalparent.R;
+
+public class HelpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_page);
+    }
+
+}
